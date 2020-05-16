d63655f5799df8867871df20578e2fd2ddfe9a76
davidf2 <dferrersmx@gmail.com>
2020-05-16 21:31:42 +0200

    Add files via upload
    Añadido codigo para identificar los caracteres mediante KNN y las imagenes que usa como dataset

--- matriculas_vc.m
+++ matriculas_vc.m
@@ -1,5 +1,6 @@
 clear all
 close all
+clc
 
 PATH = "/media/david/729A72279A71E84D/Users/David/Desktop/Enginyeria-Informatica/VC/practiques/Imatges matricules/";
 image = uint16(imread(PATH + "Imatge1-blau.jpg"));
@@ -10,55 +11,75 @@ segmented = segment(image);
 % Cortamos cada caracter de la matricula y lo guardamos en un array.
 number_plate = crop(segmented);
 
-%figure, imshow(number_plate{3},[]);
-
-% Cargamos los modelos de letras para comparar.
-models = load_models();
-%figure, imshow(models{28}, []);
-
 % Mostramos la imagen segmentada.
-figure, imshow(segmented, []);
+figure, imshowpair(uint8(image), segmented, 'montage');
 
-% Identificamos cada uno de los caracteres de la matricula.
-for i = 1: size(number_plate)
-    plate_text(i) = detect(models, number_plate{i});
-end
+[props, class] = load_models("matriculas", 11);
+
+X = props;
+Y = class;
+% Entrena a un clasificador de vecinos 3 más cercanos.
+Mdl = fitcknn(X,Y,'NumNeighbors',3);
+
+% Llamamos a la funcion para detectar cada caracter de la matricula
+plate_text = detect(Mdl, number_plate);
 
 % Mostramos el resultado por el terminal.
 disp(string(plate_text));
 
 % Función que a partir de una imagen detecta el caracter que le
 % correpsonde.
-function character = detect(models, letter)
-    comp = zeros(1, size(models, 1)); % Inicializamos el vector.
-    
-    % Por cada elemento del vector comp
-    for i = 1 : size(models,1)
-        % Comparamos las dos matrices mediante su coeficiente de
-        % correlación
-        comp(i) = corr2(models{i}, letter); 
-    end
-    
-    % Buscamos la mayor igualdad de las almacenadas.
-    winer=find(comp==max(comp));
-    
-    % Comprovamos a que caracter ascii pertenece.
-    if(winer <= 26) 
-        character = char(winer+64);
-    else
-        character = char(winer-26+47);
+function result = detect(model, plate)
+    result = "";
+    for i = 1 : size(plate,1)
+        % Extraemos el vector con el histograma de gradientes 
+        featureVector = double(extractHOGFeatures(plate{i}));
+        character = predict(model, featureVector);
+        result = result + string(character);
     end
 end
 
-% Funcion para cargar las letras que serviran de modelo.
-function models = load_models()
-    I = imread("./fuente1_matriculak.png"); % Leemos la imagen.
+function [props, class] = load_models(folder, num_models)
+    I = imread(folder + "/fuente"+1+"_matricula.png"); % Leemos la imagen.
     % Binarizamos la imagen y obtenemos su complementaria.
     bw = imcomplement(imbinarize(rgb2gray(I)));
+    % Recortamos cada caracter de la imagen y los guardamos en un array
+    % ademas cargamos todas las propiedades y sus clases en dos matrices.
+    [props, class] = load_properties(crop(bw));
     
-    % Recortamos cada caracter de la imagen y los guardamos en un array.
-    models = crop(bw);
+    for i = 2 : num_models
+        I = imread(folder + "/fuente"+i+"_matricula.png"); % Leemos la imagen.
+        % Binarizamos la imagen y obtenemos su complementaria.
+        bw = imcomplement(imbinarize(rgb2gray(I)));
+        % Recortamos cada caracter de la imagen y los guardamos en un array
+        % ademas cargamos todas las propiedades y sus clases en dos matrices.
+        [p, c] = load_properties(crop(bw));
+        props = [props ; p];
+        class = [class ; c];
+    end
+    
 end
+
+% Funcion para cargar las letras que serviran de modelo.
+function [props, class] = load_properties(models)
+    class = ["A";"B";"C";"D";"E";"F";"G";"H";"I";"J";"K";"L";"M";"N";"O"];
+    class = [class ; "P";"Q";"R";"S";"T";"U";"V";"W";"X";"Y";"Z"];
+    class = [class ; "0";"1";"2";"3";"4";"5";"6";"7";"8";"9"];
+    
+    
+    featureVector = double(extractHOGFeatures(models{1}));
+    [r, c] = size(featureVector);
+    
+    props = double(zeros(size(models,1), c)); % Inicializamos la matriz
+    props(1, :) = featureVector;
+    for i = 2 : size(models, 1)
+        % Guardamos el vector de HOG de la imagen 
+        featureVector = double(extractHOGFeatures(models{i}));
+        props(i, :) = featureVector;
+        
+    end
+end
+
 
 % Función para segmentar los caracteres de la matricula.
 function I2 = segment(I)
@@ -92,11 +113,9 @@ function words_array = crop(I)
     regions = regionprops(I, 'boundingbox');
     words_array = cell(numel(regions)); % Inicializamos word_array.
     for i = 1: numel(regions)
-        % Guardamos la fila correspondiente a i de la matriz regions.
-        region = regions(i,:);
         % Recortamos el caracter segun su BoundingBox i cambiamos su tamaño
         % por uno fijo.
-        words_array{i} = imresize(imcrop(I,region.BoundingBox), [128, 64]);
+        words_array{i} = imresize(imcrop(I,regions(i).BoundingBox), [52, 42]);
     end
 end
 
